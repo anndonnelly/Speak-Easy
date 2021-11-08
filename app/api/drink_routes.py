cfb9fe28bfcdfe38354b4cc4427368972778c899
James Thompson <thompsonjames0120@gmail.com>
2021-11-08 14:36:17 -0600

    error on post form for drinks

--- app/api/drink_routes.py
+++ app/api/drink_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, redirect
+from app.forms.drink_form import DrinkForm
 from app.models.drink import Drink, db
 
 drink_routes = Blueprint("drinks", __name__, url_prefix="/drinks")
@@ -6,9 +7,20 @@ drink_routes = Blueprint("drinks", __name__, url_prefix="/drinks")
 
 @drink_routes.route("", methods=["GET, POST"])
 def load_drinks():
-  if method == "POST":
-    pass
+  form = DrinkForm
+  if form.validate_on_submit():
+    data = form.data
+    new_drink = Drink(
+      name = data["name"],
+      description = data["description"],
+      image = data["description"],
+      abv = data["abv"]
+    )
+    db.session.add(new_drink)
+    db.session.commit()
+    return new_drink.to_dict()
   return {drink.id: drink.to_dict() for drink in Drink.query.all()}
+
 
 
 @drink_routes.route("/<int:id>")
