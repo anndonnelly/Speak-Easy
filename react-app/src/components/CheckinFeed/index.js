cdad11d9d63e7459d69f3a45aec482e4214b336f
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-11-08 18:14:08 -0500

    edit checkin in progress

---
+++ react-app/src/components/CheckinFeed/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { getCheckinsThunk } from "../../store/checkins";
+import { useDispatch, useSelector } from "react-redux";
+import CheckinCard from "../CheckinCard/CheckinCard";
+import "./CheckinFeed.css";
+
+function CheckinsFeed() {
+  const dispatch = useDispatch();
+
+
+  const checkins = useSelector((state) => Object.values(state.checkins));
+
+  useEffect(() => {
+    dispatch(getCheckinsThunk());
+  }, [dispatch]);
+
+  return (
+    <>
+   
+        {checkins.map((checkin) => (
+            <CheckinCard key={checkin.id} checkin={checkin}/>
+        ))}
+          
+        
+    </>
+  );
+}
+
+export default CheckinsFeed;
