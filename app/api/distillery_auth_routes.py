ac72e25a138d8e88dda9a24782fe541f9c975abf
James Thompson <thompsonjames0120@gmail.com>
2021-11-08 17:45:14 -0600

    password not hashing for distilleries, login for distilleries buggy

--- app/api/distillery_auth_routes.py
+++ app/api/distillery_auth_routes.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db
-from app.forms import LoginForm
-from app.forms import SignUpForm
+from app.models import Distillery, db
+from app.forms import DistillerySignUpForm, DistilleryLoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 
-auth_routes = Blueprint('auth', __name__)
+distillery_auth_routes = Blueprint('distillery_auth', __name__, url_prefix="/distilleries")
 
 
 def validation_errors_to_error_messages(validation_errors):
@@ -18,34 +17,34 @@ def validation_errors_to_error_messages(validation_errors):
     return errorMessages
 
 
-@auth_routes.route('/')
+@distillery_auth_routes.route('')
 def authenticate():
     """
-    Authenticates a user.
+    Authenticates a Distillery.
     """
     if current_user.is_authenticated:
         return current_user.to_dict()
     return {'errors': ['Unauthorized']}
 
 
-@auth_routes.route('/login', methods=['POST'])
+@distillery_auth_routes.route('/login', methods=['POST'])
 def login():
     """
-    Logs a user in
+    Logs a Distillery in
     """
-    form = LoginForm()
+    form = DistilleryLoginForm()
     # Get the csrf_token from the request cookie and put it into the
     # form manually to validate_on_submit can be used
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
-        user = User.query.filter(User.email == form.data['email']).first()
-        login_user(user)
-        return user.to_dict()
+        distillery = Distillery.query.filter(Distillery.email == form.data['email']).first()
+        login_user(distillery)
+        return distillery.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
-@auth_routes.route('/logout')
+@distillery_auth_routes.route('/logout')
 def logout():
     """
     Logs a user out
@@ -54,27 +53,31 @@ def logout():
     return {'message': 'User logged out'}
 
 
-@auth_routes.route('/signup', methods=['POST'])
+@distillery_auth_routes.route('/signup', methods=['POST'])
 def sign_up():
     """
     Creates a new user and logs them in
     """
-    form = SignUpForm()
+    form = DistillerySignUpForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        user = User(
-            username=form.data['username'],
+        distillery = Distillery(
+            name=form.data['name'],
             email=form.data['email'],
-            password=form.data['password']
+            hashed_password=form.data['hashed_password'],
+            street=form.data['street'],
+            city=form.data['city'],
+            state=form.data['state'],
+            logo=form.data['logo']
         )
-        db.session.add(user)
+        db.session.add(distillery)
         db.session.commit()
-        login_user(user)
-        return user.to_dict()
+        login_user(distillery)
+        return distillery.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
-@auth_routes.route('/unauthorized')
+@distillery_auth_routes.route('/unauthorized')
 def unauthorized():
     """
     Returns unauthorized JSON when flask-login authentication fails
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,5 @@
-from .login_form import LoginForm
-from .signup_form import SignUpForm
+from .user_login_form import LoginForm
+from .user_signup_form import SignUpForm
+from .distillery_signup_form import DistillerySignUpForm
+from .distillery_login_form import DistilleryLoginForm
 from .checkin_form import CheckinForm
