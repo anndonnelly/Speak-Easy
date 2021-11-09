0e1902bf75f2f0dff91632f651c23a38c823719a
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-11-08 21:20:17 -0500

    edit form centered

--- react-app/src/components/CheckinCard/CheckinCard.js
+++ react-app/src/components/CheckinCard/CheckinCard.js
@@ -47,65 +47,67 @@ function CheckinCard({checkin}) {
 
   if(edit){
       return (
-        <form onSubmit={handleEdit}>
-          {/* <ul>
-            {errors.map((error) => (
-              <li key={error}>{error}</li>
-            ))}
-          </ul> */}
-          <div>
-            <label>Review</label>
-            <textarea
-              name="review"
-              value={editReview}
-              onChange={(e) => setEditReview(e.target.value)}
-            />
-          </div>
-          {/* <div>
-            <label>Location</label>
-            <select
-              value={editLocation}
-              onChange={(e) => setEditLocation(e.target.value)}
-            >
-              {eventLocations.map((location) => (
-                <option value={location.id} key={location.id}>
-                  {location.name}
+          <div className="edit-card">
+            <form onSubmit={handleEdit}>
+            {/* <ul>
+                {errors.map((error) => (
+                <li key={error}>{error}</li>
+                ))}
+            </ul> */}
+            <div>
+                <label>Review</label>
+                <textarea
+                name="review"
+                value={editReview}
+                onChange={(e) => setEditReview(e.target.value)}
+                />
+            </div>
+            {/* <div>
+                <label>Location</label>
+                <select
+                value={editLocation}
+                onChange={(e) => setEditLocation(e.target.value)}
+                >
+                {eventLocations.map((location) => (
+                    <option value={location.id} key={location.id}>
+                    {location.name}
+                    </option>
+                ))}
+                </select>
+            </div> */}
+            <div>
+                <label>Drink</label>
+                {/* <select value={editDrinkId} onChange={(e) => setEditDrinkId(e.target.value)}>
+                {eventLocations.map((location) => (
+                    <option value={location.id} key={location.id}>
+                    {location.name}
+                    </option>
+                ))}
+                </select> */}
+            </div>
+            <div>
+                <label>Rating</label>
+                <select
+                name="rating"
+                value={editRating}
+                onChange={(e) => setEditRating(e.target.value)}
+                >
+                <option value="" disabled>
+                    --Rating--
                 </option>
-              ))}
-            </select>
-          </div> */}
-          <div>
-            <label>Drink</label>
-            {/* <select value={editDrinkId} onChange={(e) => setEditDrinkId(e.target.value)}>
-              {eventLocations.map((location) => (
-                <option value={location.id} key={location.id}>
-                  {location.name}
-                </option>
-              ))}
-            </select> */}
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                </select>
+            </div>
+            <div>
+                <button >Update</button>
+            </div>
+            </form>
           </div>
-          <div>
-            <label>Rating</label>
-            <select
-              name="rating"
-              value={editRating}
-              onChange={(e) => setEditRating(e.target.value)}
-            >
-              <option value="" disabled>
-                --Rating--
-              </option>
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </div>
-          <div>
-            <button >Update</button>
-          </div>
-        </form>
       );
   }
 
