cd6d3628e8d612e58b6d1610d9574b08466f5881
Robert Kauth <kauthr@gmail.com>
2021-11-08 17:22:38 -0800

    added reducers and actions to window

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -7,12 +7,16 @@ import configureStore from "./store";
 import { ModalProvider } from "./context/Modal";
 import { BrowserRouter } from "react-router-dom";
 import * as drinksActions from "./store/drinks";
+import * as distilleryActions from "./store/distillery_session";
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
     window.store = store;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     window.drinksActions = drinksActions;
+    window.distilleryActions = distilleryActions;
 }
 
 ReactDOM.render(
