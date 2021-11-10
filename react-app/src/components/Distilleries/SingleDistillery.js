b4896d88464485b9324cec18579d444342d2ee89
James Thompson <thompsonjames0120@gmail.com>
2021-11-09 20:46:33 -0600

    single page basic info

--- react-app/src/components/Distilleries/SingleDistillery.js
+++ react-app/src/components/Distilleries/SingleDistillery.js
@@ -4,21 +4,29 @@ import { showModal, setCurrentModal } from "../../store/modal";
 import { loadOneDistillery } from "../../store/distilleries";
 import { useParams } from "react-router-dom";
 
-
 const SingleDistillery = () => {
-    const {distilleryId} = useParams()
-    console.log(distilleryId)
-    const dispatch = useDispatch();
-    const distillery = useSelector(state => state.distilleries)
-    console.log("sdfghjkl;", distillery)
+  const { distilleryId } = useParams();
+  console.log(distilleryId);
+  const dispatch = useDispatch();
+  const distillery = useSelector((state) => state.distilleries);
+  console.log("sdfghjkl;", distillery);
 
-    useEffect(()=>{
-        dispatch(loadOneDistillery(distilleryId))
-    }, [dispatch, distilleryId])
+  useEffect(() => {
+    dispatch(loadOneDistillery(distilleryId));
+  }, [dispatch, distilleryId]);
 
-    return(
-        <h1>Hello from single distillery</h1>
-    )
-}
+  return (
+    <div>
+      <div>
+        <img src={distillery.logo} alt="Distillery Logo" />
+      </div>
+      <h1>{distillery.name}</h1>
+      <div>{distillery.street}</div>
+      <div>{distillery.city}</div>
+      <div>{distillery.state}</div>
+      <div>{distillery.checkin_ids}</div>
+    </div>
+  );
+};
 
-export default SingleDistillery
+export default SingleDistillery;
