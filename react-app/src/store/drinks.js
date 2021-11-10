91b69c9b739d1440e314653799fc9da0bb516665
Robert Kauth <kauthr@gmail.com>
2021-11-09 16:07:44 -0800

    fixed drinks initial state

--- react-app/src/store/drinks.js
+++ react-app/src/store/drinks.js
@@ -5,6 +5,7 @@ const UPDATE_DRINK = "drink/UPDATE_DRINK";
 const REMOVE_DRINK = "drink/REMOVE_DRINK";
 
 /*-------------ACTIONS-------------*/
+
 const loadDrinks = (drinks) => {
     return {
         type: LOAD_DRINKS,
@@ -34,6 +35,7 @@ const removeDrink = (drinkId) => {
 };
 
 /*-------------THUNK CREATORS-------------*/
+
 export const load = () => async (dispatch) => {
     const res = await fetch("/api/drinks");
     if (res.ok) {
