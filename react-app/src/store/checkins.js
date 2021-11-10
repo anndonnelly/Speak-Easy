1ea38ad9a79c0433a2e56aeb91272037f914643f
Robert Kauth <kauthr@gmail.com>
2021-11-10 10:45:52 -0800

    fixed checkins error using object.values

--- react-app/src/store/checkins.js
+++ react-app/src/store/checkins.js
@@ -1,120 +1,115 @@
 //CONSTANTS
-const GET_CHECKINS= "checkins/LOAD";
+const GET_CHECKINS = "checkins/LOAD";
 const ADD_CHECKIN = "checkins/ADD";
 const EDIT_CHECKIN = "checkins/EDIT";
 const DELETE_CHECKIN = "checkins/DELETE";
 
 //ACTION CREATORS
-const getCheckinsAction = (checkin) => {
-  return {
-    type: GET_CHECKINS,
-    payload: checkin,
-  };
+const getCheckinsAction = (checkins) => {
+    return {
+        type: GET_CHECKINS,
+        checkins,
+    };
 };
 
 const addCheckinAction = (checkin) => {
-  return {
-    type: ADD_CHECKIN,
-    payload: checkin,
-  };
+    return {
+        type: ADD_CHECKIN,
+        payload: checkin,
+    };
 };
 
 const editCheckinAction = (checkin) => {
-  return {
-    type: EDIT_CHECKIN,
-    payload: checkin,
-  };
+    return {
+        type: EDIT_CHECKIN,
+        payload: checkin,
+    };
 };
 
 const deleteCheckinAction = (checkin) => {
-  return {
-    type: DELETE_CHECKIN,
-    payload: checkin,
-  };
+    return {
+        type: DELETE_CHECKIN,
+        payload: checkin,
+    };
 };
 
 //THUNKS
 export const getCheckinsThunk = () => async (dispatch) => {
-  const response = await fetch("/api/checkins");
-  let checkins_obj = await response.json();
-
-  if (response.ok) {
-    dispatch(getCheckinsAction(checkins_obj));
-  } else {
-    return {ok:false}
-  }
+    const response = await fetch("/api/checkins");
+    if (response.ok) {
+        let checkins_obj = await response.json();
+        dispatch(getCheckinsAction(checkins_obj));
+    } else {
+        return { ok: false };
+    }
 };
 
 export const createCheckinsThunk = (checkin) => async (dispatch) => {
-  const res = await fetch("/api/checkins/new", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(checkin),
-  });
-  if (res.ok) {
-    let newCheckin = await res.json();
-    dispatch(addCheckinAction(newCheckin));
-    // getting all actions and turning it into a payload
-  } else {
-    //handle errors
-  }
+    const res = await fetch("/api/checkins/new", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkin),
+    });
+    if (res.ok) {
+        let newCheckin = await res.json();
+        dispatch(addCheckinAction(newCheckin));
+        // getting all actions and turning it into a payload
+    } else {
+        //handle errors
+    }
 };
 
 export const editCheckinsThunk = (id, checkin) => async (dispatch) => {
-  const res = await fetch(`/api/checkins/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(checkin),
-  });
+    const res = await fetch(`/api/checkins/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkin),
+    });
 
-  if (res.ok) {
-    let editCheckin = await res.json();
-    dispatch(editCheckinAction(editCheckin));
-  } else {
-    //handle errors
-  }
+    if (res.ok) {
+        let editCheckin = await res.json();
+        dispatch(editCheckinAction(editCheckin));
+    } else {
+        //handle errors
+    }
 };
 
 export const deleteCheckinsThunk = (id) => async (dispatch) => {
-
-  const response = await fetch(`/api/checkins/${id}`,
-    {method: "DELETE"}
-  );
-  if (response.ok) {
-    dispatch(deleteCheckinAction(id));
-  } else {
-    //error stuff
-  }
+    const response = await fetch(`/api/checkins/${id}`, { method: "DELETE" });
+    if (response.ok) {
+        dispatch(deleteCheckinAction(id));
+    } else {
+        //error stuff
+    }
 };
 
 //REDUCER
 const initialState = {};
 export default function checkinsReducer(state = initialState, action) {
-  const newState = { ...state };
-  switch (action.type) {
-    case GET_CHECKINS:
-        return action.payload;
-        // const allCheckins = {};
-        // action.payload.forEach(checkin => {
-        //   allCheckins[checkin.id] = checkin
-        // })
-    //   return { ...state, ...allCheckins };
-    case ADD_CHECKIN:
-      newState[action.payload.id] = action.payload;
-      return newState;
-    case EDIT_CHECKIN:
-      newState[action.payload.id] = action.payload;
-      return newState;
-    case DELETE_CHECKIN:
-      delete newState[action.payload];
-      return newState;
-    default:
-      return state;
-  }
+    const newState = { ...state };
+    switch (action.type) {
+        case GET_CHECKINS:
+            const allCheckins = {};
+            Object.values(action.checkins).forEach((checkin) => {
+                allCheckins[checkin.id] = checkin;
+            });
+            return { ...state, ...allCheckins };
+        case ADD_CHECKIN:
+            newState[action.payload.id] = action.payload;
+            return newState;
+        case EDIT_CHECKIN:
+            newState[action.payload.id] = action.payload;
+            return newState;
+        case DELETE_CHECKIN:
+            delete newState[action.payload];
+            return newState;
+        default:
+            return state;
+    }
 }
 
 // normalizing data inside reducer
