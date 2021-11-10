9045c10437caacb8105d0df574da339b755c5b9f
Robert Kauth <kauthr@gmail.com>
2021-11-10 15:39:18 -0800

    added checkin modal to distillery drinks feed

--- react-app/src/components/Distilleries/DrinkCard.js
+++ react-app/src/components/Distilleries/DrinkCard.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import CreateCheckin from "../../components/CreateCheckin/index";
+import { showModal, setCurrentModal } from "../../store/modal";
 import styles from "./DrinkCard.module.css";
 const DrinkCard = ({ drink }) => {
+    const dispatch = useDispatch();
+
+    const checkin = (e) => {
+        e.preventDefault();
+        dispatch(setCurrentModal(CreateCheckin));
+        dispatch(showModal());
+    };
     return (
         <div className={styles.drinkContainer}>
             <div className={styles.name}>{drink.name}</div>
@@ -8,6 +18,9 @@ const DrinkCard = ({ drink }) => {
                 <img className={styles.img} src={drink.image} alt="" />
             </div>
             <div className={styles.rating}>Rating: {drink.rating}</div>
+            <div>
+                <button onClick={checkin}>+</button>
+            </div>
         </div>
     );
 };
