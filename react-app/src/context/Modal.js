ecd5f1e7ddd493e394d801eb6766b0b3a7e89892
Robert Kauth <kauthr@gmail.com>
2021-11-13 10:20:23 -0800

    converted modal css to module

--- react-app/src/context/Modal.js
+++ react-app/src/context/Modal.js
@@ -1,35 +1,37 @@
 import React, { useContext, useRef, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import "./Modal.css";
+import styles from "./Modal.module.css";
 
 const ModalContext = React.createContext();
 
 export function ModalProvider({ children }) {
-  const modalRef = useRef();
-  const [value, setValue] = useState();
+    const modalRef = useRef();
+    const [value, setValue] = useState();
 
-  useEffect(() => {
-    setValue(modalRef.current);
-  }, []);
+    useEffect(() => {
+        setValue(modalRef.current);
+    }, []);
 
-  return (
-    <>
-      <ModalContext.Provider value={value}>{children}</ModalContext.Provider>
-      <div ref={modalRef} />
-    </>
-  );
+    return (
+        <>
+            <ModalContext.Provider value={value}>
+                {children}
+            </ModalContext.Provider>
+            <div ref={modalRef} />
+        </>
+    );
 }
 
 export function Modal({ onClose, children }) {
-  const modalNode = useContext(ModalContext);
+    const modalNode = useContext(ModalContext);
 
-  if (!modalNode) return null;
+    if (!modalNode) return null;
 
-  return ReactDOM.createPortal(
-    <div id="modal">
-      <div id="modal-background" onClick={onClose} />
-      <div id="modal-content">{children}</div>
-    </div>,
-    modalNode
-  );
+    return ReactDOM.createPortal(
+        <div className={styles.modal}>
+            <div className={styles.modalBackground} onClick={onClose} />
+            <div className={styles.modalContent}>{children}</div>
+        </div>,
+        modalNode
+    );
 }
