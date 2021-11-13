70702d634993f59532e2cbc323ca1bdc2ae5e1b4
Robert Kauth <kauthr@gmail.com>
2021-11-13 10:16:04 -0800

    fixed login modal size and coverted css to module

--- react-app/src/components/CheckinModal/index.js
+++ react-app/src/components/CheckinModal/index.js
@@ -1,23 +1,29 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import { useSelector, useDispatch } from "react-redux";
-import "./CheckinModal.css";
-import { hideModal } from '../../store/modal';
+import styles from "./CheckinModal.module.css";
+import { hideModal } from "../../store/modal";
 
-export default function Modal(){
-    const dispatch = useDispatch()
-    const mount = useSelector(state => state.modal.mount)
-    const display = useSelector(state => state.modal.display)
-    const Current = useSelector(state => state.modal.current)
+export default function Modal() {
+    const dispatch = useDispatch();
+    const mount = useSelector((state) => state.modal.mount);
+    const display = useSelector((state) => state.modal.display);
+    const Current = useSelector((state) => state.modal.current);
 
     const onClose = () => {
-        dispatch(hideModal())
-    }
-
-    return mount && display && ReactDOM.createPortal (
-       <div onClick={onClose} className="modal-background">
-           <div onClick={e => e.stopPropagation()}className="modal-content">
-               <Current/>
-           </div>
-       </div> 
-    , mount)
+        dispatch(hideModal());
+    };
+    return (
+        mount &&
+        display &&
+        ReactDOM.createPortal(
+            <div onClick={onClose} className={styles.modalBackground}>
+                <div
+                    onClick={(e) => e.stopPropagation()}
+                    className={styles.modalContent}>
+                    <Current />
+                </div>
+            </div>,
+            mount
+        )
+    );
 }
