cf510eb5d698954e68639892da0ccde5b0636fdd
Robert Kauth <kauthr@gmail.com>
2021-11-12 17:37:30 -0800

    removed unused components from loginform

--- react-app/src/components/LoginModal/index.js
+++ react-app/src/components/LoginModal/index.js
@@ -7,13 +7,7 @@ import styles from "./LoginModal.module.css";
 import { showModal, setCurrentModal } from "../../store/modal";
 
 function LoginFormModal() {
-    // const [showModal, setShowModal] = useState(false);
-    const user = useSelector((state) => state.session.user);
     const dispatch = useDispatch();
-
-    if (user) {
-        return <>{/* <ProfileButton></ProfileButton> */}</>;
-    }
 
     const handleLogin = (e) => {
         e.preventDefault();
@@ -26,11 +20,6 @@ function LoginFormModal() {
             <button className={styles.navButton} onClick={handleLogin}>
                 Log In
             </button>
-            {/* {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <LoginForm setShowModal={setShowModal} />
-                </Modal> */}
-            {/* )} */}
         </>
     );
 }
