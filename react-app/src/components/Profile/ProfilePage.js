6e51ee59ba8fff827d9ddf7f4d2e16b8106f2b03
Robert Kauth <kauthr@gmail.com>
2021-11-14 10:56:27 -0800

    adjusted profile page button and username positioning

--- react-app/src/components/Profile/ProfilePage.js
+++ react-app/src/components/Profile/ProfilePage.js
@@ -19,12 +19,14 @@ const ProfilePage = () => {
 
     return (
         <main className={styles.profile}>
-            <div>
-                <button onClick={createDistilleryModal}>
+            <div className={styles.btnWrapper}>
+                <button
+                    className={styles.start}
+                    onClick={createDistilleryModal}>
                     Start A Distillery
                 </button>
             </div>
-            <h1>{user.username}</h1>
+            <div className={styles.userName}>{user.username}</div>
             <ProfileFeedSelection />
             <InternalFooter />
         </main>
